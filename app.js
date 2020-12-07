96f24a700cd8f74f356758f65a3f4bea5ff6e753
Brent Groves <brent.groves@gmail.com>
2020-12-07 15:59:52 -0500

    Initial Creation

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+
+/*
+I don't have any code for this but there is a CLI 
+npx ftp-srv ftp://0.0.0.0:9876 --root ~/Documents
+*/
+/*
+Sample code that has not been tested yet
+*/
 const FtpSrv = require('ftp-srv');
 const ftpServer = new FtpSrv({ options ... });
 // "ftp://127.0.0.1:21"
